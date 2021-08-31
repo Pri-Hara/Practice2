226bb995ad10a35fb97ea400e58e0c86bba59bc6
hara-lab <naoki_1014.shisho@outlook.jp>
2021-09-01 00:27:07 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by 原直輝 on 2021/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
